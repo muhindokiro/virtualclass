42391079428c8576a9f2caa883975ee609b1bd2d
Muhindo Kiro <blaize1143@gmail.com>
2022-11-21 10:48:26 +0300

    add new files

---
+++ virtual/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Article
+
+# Register models here.
+admin.site.register(Article)
+# admin.site.register(User)
+# admin.site.register(Student)
+# admin.site.register(Lecturer)
