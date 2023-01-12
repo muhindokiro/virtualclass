eb2530f05fc2207050c322f373627575bcc709d9
kawira420 <kawira9muraa@gmail.com>
2023-01-12 10:28:39 +0300

    new_modification

--- virtual/admin.py
+++ virtual/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# admin.site.register(File)
+admin.site.register(File)
 # admin.site.register(Customer)
 # used to display your models in the django admin panel
