878b1bcfbac9080f6dddffe15bcde74520ee024d
muhindokiro <blaize1143@gmail.com>
2023-01-24 08:20:11 +0300

    create new model profile foruser info

--- virtual/models.py
+++ virtual/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Customer(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpeg', upload_to='profile_pics/')
+    school = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    profession = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return f' {self.user.username} Profile'
     
 class File(models.Model):
     fullname = models.CharField(max_length=50)
