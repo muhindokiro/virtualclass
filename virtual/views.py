1ed4e8fa9b3059716409365b8aec07ca55d92968
muhindokiro <blaize1143@gmail.com>
2023-02-13 13:48:35 +0300

    create new view

--- virtual/views.py
+++ virtual/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required,permission_required
 from .decorators import allowed_users,admin_only
 from django.views.decorators import gzip
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse,HttpResponse
 import cv2
 import threading
 
@@ -59,6 +59,7 @@ def cameraView(request):
 @allowed_users(allowed_roles=['student'])
 def studentPage(request):
     files = File.objects.all()
+    print('WHEN IS IT CALLED111')
     return render(request, "student.html", {'files': files})
 
 
@@ -110,10 +111,14 @@ def loginPage(request):
         return render(request, "login.html", context)
 
 
+
 def logoutUser(request):
     logout(request)
     return redirect("login")
 
+def notify_lecture(self):
+    print('TESTING THE PRINT')
+    
 @login_required(login_url="login")
 @allowed_users(allowed_roles=['lecturer'])
 def upload_file(request):
@@ -121,6 +126,7 @@ def upload_file(request):
         form =  FileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request)
             return redirect('lecturer')
     else:
         form = FileForm()
@@ -152,4 +158,18 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
+def userNotification(request):
+    user = request.user
+    files = File.objects.filter(user=user).order_by('-sate')
 
+    template = loader.get_template('student.html')
+
+    context = {
+        'files': files,
+    }
+
+    print('TESTING THE DOWNLOAD!!!')
+
+    
+    return HttpResponse(template.render(context, request))
+    # return render(request, 'student.html')
